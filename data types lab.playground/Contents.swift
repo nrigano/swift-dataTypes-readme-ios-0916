e4b60a27e351cd8c3f3fad6b6bf45597758145af
Nick Rigano <nrigano@Executor.local>
2016-09-16 11:33:40 -0400

    Complete

---
+++ data types lab.playground/Contents.swift
@@ -0,0 +1,17 @@
+let thirdPlanet="Earth"
+var ninthPlanet: String
+ninthPlanet = "Pluto"
+let fifthPlanet: String = "Jupiter"
+// fifthPlanet is a constant of type String
+
+let sixthPlanet: String = "Saturn"
+// sixthPlanet is a constant of type String
+
+let seventhPlanet = "Uranus"
+// seventhPlanet is a constant of type String
+
+let numberOfCountriesInAfrica = 54
+
+let numberOfMoonsOfSaturn = 62
+
+print ("There are \(numberOfMoonsOfSaturn) moons orbiting \(sixthPlanet)")
